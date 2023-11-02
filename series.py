475da5c1e89549d6705d52c66cb32651dba3200d
Martina Garcia <martinagg@myuax.com>
2023-11-02 06:28:42 +0100

    .

---
+++ series.py
@@ -0,0 +1,13 @@
+""""
+
+Se jugarán n series y en tu grupo sois k amigos.
+
+Conoces la hora de inicio y la hora de finalización de cada serie. ¿Cuál es el número
+máximo total de series que podéis ver por completo si actuáis de manera óptima?
+Entrada:
+
+La primera línea de entrada tiene dos enteros n y k: el número de series y el número
+de amigos.
+Después de esto, hay n líneas que describen las series
+
+""""
